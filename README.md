1dddd55a4243cbc69a1018c65a7c087006154110
FranQie93 <franq.santos.fs@gmail.com>
2023-08-17 07:02:08 -0700

    Update

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Shell project by Francis Quaye (franq.santos.fs@gmail.com) and Yussif Badu (yussif.59@gmail.com )
---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - duplicate a string
+ *@dest: the target string that will be duplicated to
+ *@src: the original string
+ *@n: the number of characters intended for duplication
+ *Return: the joined string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - joined two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be fully used
+ *Return: the joined string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - finds a character within a string
+ *@s: the string that is to be analyzed
+ *@c: the specific character to search for
+ *Return: (s) a reference to the memory region s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
